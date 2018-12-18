41b9b7ca092aa6ab3c1f85ab762a1485951206f2
Khan <bmkhan@URBANSCIENCE.com>
2018-12-18 13:17:38 +0100

    Push to branch-1

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,7 +2,10 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hi, Git Testing");
+		printMessage();
 	}
 
+	private static void printMessage() {
+		System.out.println("Hi, Git Testing");
+	}
 }
